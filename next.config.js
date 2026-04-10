a8bc1fc1a0e56d6ebb1ad7484f929871ec1c7e92
agnumstudio-hub <agnumstudio@gmail.com>
2026-04-10 11:23:05 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ["en", "fr", "it", "dt", "pt", "es"],
+    locales: ["en", "fr", "it", "de", "pt", "es"],
     defaultLocale: "en",
     localeDetection: false,
   },
